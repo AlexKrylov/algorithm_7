ac0162581653a2b17420c03eb1bc8f87e26120fe
AlexKrylov <33526162+AlexKrylov@users.noreply.github.com>
2017-12-29 01:26:56 +0300

    DFS and BFS

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Main {
+    static int vertexCount;
+    static int edgeCount;
+    static boolean[][] matrixAdjacency;
+    static boolean[] used;
+
+    public static void main(String[] args) {
+
+    }
+    //Поиск в глубину (Depth-first search, DFS)
+    public static void dfs(int v) {
+        used[v] = true;
+        for (int nv = 0; nv < vertexCount; nv++)
+            if (!used[nv] && matrixAdjacency[v][nv])
+                dfs(nv);
+    }
+
+    //Поиск в ширину (Breadth-first search, BFS)
+    public static void bfs(int v) {
+        boolean[] used = new boolean[vertexCount];
+        int[] queue = new int[vertexCount];
+        int first = 0;
+        int last = 0;
+        used[v] = true;
+        queue[last++] = v;
+        while (first < last) {
+            v = queue[first++];
+            for (int nv = 0; nv < vertexCount; nv++) {
+                if (!used[nv] && matrixAdjacency[v][nv]) {
+                    used[nv] = true;
+                    queue[last++] = nv;
+                }
+            }
+        }
+    }
 }
